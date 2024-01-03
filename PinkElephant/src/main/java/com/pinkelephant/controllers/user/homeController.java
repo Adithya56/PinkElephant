21e430a704f71518a606d313c4cf19a7635661d0
ADITHYA THANDRA <85007575+Adithya56@users.noreply.github.com>
2024-01-03 22:04:31 +0530

    Add files via upload

---
+++ PinkElephant/src/main/java/com/pinkelephant/controllers/user/homeController.java
@@ -0,0 +1,14 @@
+package com.pinkelephant.controllers.user;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class homeController {
+	// url mapping for home page
+		@RequestMapping(value = "/", method = RequestMethod.GET)
+		public String getHomePage(Model model) {
+			// call the view
+			return "home";
+		}
+}
