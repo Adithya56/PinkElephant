ab0e805074fa6c7cc3f4fa15a95a72fb3644f970
ADITHYA THANDRA <85007575+Adithya56@users.noreply.github.com>
2024-01-03 22:09:51 +0530

    Delete PinkElephant directory

--- PinkElephant/user (1)/user.java
+++
@@ -1,9 +0,0 @@
-package com.pinkelephant.model.user;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-public class user {
-
-}
--- PinkElephant/user (2)/userLoginService.java
+++
@@ -1,9 +0,0 @@
-package com.pinkelephant.services.user;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-public class userLoginService {
-	
-}
