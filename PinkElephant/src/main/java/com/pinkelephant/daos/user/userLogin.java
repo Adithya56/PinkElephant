fb25d796320667665152747a3e104a9ef8424dc5
ADITHYA THANDRA <85007575+Adithya56@users.noreply.github.com>
2024-01-03 22:10:53 +0530

    Add files via upload

---
+++ PinkElephant/src/main/java/com/pinkelephant/daos/user/userLogin.java
@@ -0,0 +1,9 @@
+package com.pinkelephant.daos.user;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class userLogin {
+	
+}
---
+++ PinkElephant/src/main/java/com/pinkelephant/model/user/user.java
@@ -0,0 +1,9 @@
+package com.pinkelephant.model.user;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class user {
+
+}
