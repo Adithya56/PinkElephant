6817d69d1def0f81658c1cfcfb99d240c1f3930f
ADITHYA THANDRA <85007575+Adithya56@users.noreply.github.com>
2024-01-06 16:07:53 +0530

    Delete PinkElephant directory

--- PinkElephant/src/main/java/com/pinkelephant/daos/user/userLogin.java
+++
@@ -1,9 +0,0 @@
-package com.pinkelephant.daos.user;
-
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-
-public class userLogin {
-	
-}
